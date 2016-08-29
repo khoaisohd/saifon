12ea9cbb2679fe1cdc195270b187d6e4fb323416
inyigo <inigo@wego.com>
2016-08-30 00:29:08 +0800

    StarRatingsFilter component

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from '@kadira/storybook';
 function loadStories() {
   require('../stories/HotelCard.stories.js');
   require('../stories/StarRating.stories.js');
+  require('../stories/Filters/StarRatingsFilter.stories.js');
 }
 
 configure(loadStories, module);
