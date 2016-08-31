dc2702a9131995e07ff5e095b48057f6c63faf88
Tom Hoang <hoangdinhphuong89@gmail.com>
2016-09-01 01:56:58 +0800

    Clean up

--- app/components/Overlay/index.js
+++ app/components/Overlay/index.js
@@ -1,23 +1,15 @@
 import React, { PropTypes } from 'react';
 import styles from './styles.css';
 
-let lastComponent = null;
-let firstLoad = true;
-
 function Overlay(props) {
-  const { children } = props;
-  const renderComponent = children || lastComponent;
-  const loadState = firstLoad ? styles.firstLoad : styles.afterFirstLoad;
-  const displayState = children ? styles.isShown : '';
-
-  firstLoad = false;
-  lastComponent = children;
-
-  return (
-    <div className={`${styles.overlayContainer} ${displayState} ${loadState}`}>
-      {renderComponent}
-    </div>
-  );
+  if (props.children) {
+    return (
+      <div className={styles.overlayContainer}>
+        {props.children}
+      </div>
+    );
+  }
+  return <div />;
 }
 
 Overlay.propTypes = {
--- app/utils/enableOverlay.js
+++ app/utils/enableOverlay.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Overlay from 'components/Overlay';
+import appStyles from 'containers/App/styles.css';
 
 /* eslint-disable */
 
 function enableOverlay(Component) {
   return React.createClass({
     render: function() {
+      const componentStyle = this.props.children ? appStyles.noScroll : '';
       return (
         <div>
           <Overlay>
             {this.props.children}
           </Overlay>
-          <Component {...this.props} />
+          <div className={componentStyle}>
+            <Component {...this.props} />
+          </div>
         </div>
       );
     },
