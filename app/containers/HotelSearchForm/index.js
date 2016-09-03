25aa7d23aadba92b95d15c1917c8b32715e8aad9
Tom Hoang <hoangdinhphuong89@gmail.com>
2016-09-03 15:31:00 +0800

    Clean up

--- app/containers/HotelSearchForm/index.js
+++ app/containers/HotelSearchForm/index.js
@@ -7,6 +7,7 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import appStyles from 'containers/App/styles.css';
+import styles from './styles.css';
 import { pathToHotelSearch } from 'utils/routes-util';
 import { getCheckIn, getCheckOut, getLocation, getRoomsCount, getGuestsCount } from './selectors';
 
@@ -16,7 +17,7 @@ class HotelSearchForm extends React.Component { // eslint-disable-line react/pre
 
     return (
       <div>
-        <div className={appStyles.toolbar}>
+        <div className={styles.toolbar}>
           <i className={appStyles.backButton} onClick={this.context.router.goBack} />
           Hotel Search
         </div>
