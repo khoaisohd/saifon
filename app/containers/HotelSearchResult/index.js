dfd06b876f54de13b38a994022e898f6dfbbe48d
inyigo <inigo@wego.com>
2016-09-04 12:53:34 +0800

    remove enableoverlay

--- app/containers/HotelSearchResult/index.js
+++ app/containers/HotelSearchResult/index.js
@@ -1,13 +1,47 @@
 import 'containers/HotelSearchResult/Filters'; // Preload form assets
 import React, { Component } from 'react';
 import Results from './Results';
+import Modal from 'react-modal';
 
-class HotelSearchResult extends Component { // eslint-disable-line react/prefer-stateless-function
+/* eslint-disable */
+const customStyles = {
+  content : {
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    padding: 0,
+  },
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+};
+
+class HotelSearchResult extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = { isModalOpen: false };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.children) {
+      this.setState({ isModalOpen: true });
+    } else {
+      this.setState({ isModalOpen: false });
+    }
+  }
+
   render() {
     return (
       <div>
+        <Modal isOpen={this.state.isModalOpen} style={customStyles}>
+          {this.props.children}
+        </Modal>
         <Results searchParams={this.props.routeParams} />
-        {this.props.children}
       </div>
     );
   }
