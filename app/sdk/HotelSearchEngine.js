2ce028ab0b2343e99838ac4a3a00c1596572a644
inyigo <inigo@wego.com>
2016-09-07 00:35:51 +0800

    rudimentary sort

--- app/sdk/HotelSearchEngine.js
+++ app/sdk/HotelSearchEngine.js
@@ -24,15 +24,58 @@ class HotelSearchEngine {
 
   findHotels(filters = new Map(), sort, offset = 0, limit = 20) {
     return Promise.resolve(this.hotels)
-      .then((hotels) => this.filter(hotels, filters))
-      .then((hotels) => hotels.slice(offset, limit));
+      .then((hotels) => this.filterHotels(hotels, filters))
+      .then((filteredHotels) => this.sortHotels(filteredHotels, sort))
+      .then((sortedHotels) => sortedHotels.slice(offset, limit));
   }
 
-  filter(hotels, filters) {
+  filterHotels(hotels, filters) {
     const starRatings = filters.get('starRatings');
     return hotels.filter((hotel) =>
       this.hasTheStarsAligned(hotel, starRatings)
     );
+  }
+
+  sortHotels(hotels, sortData) {
+    const sorter = this.sortBy(sortData);
+    return hotels.sort(sorter);
+  }
+
+  sortBy(sortData) {
+    let getSortValue;
+    switch (sortData.get('property')) {
+      case 'PRICE':
+        getSortValue = this.getCheapestAmount;
+        break;
+      case 'REVIEW_SCORE':
+        getSortValue = this.getReviewScore;
+        break;
+      case 'STAR_RATING':
+        getSortValue = this.getStarRating;
+        break;
+      default:
+        break;
+    }
+    return (hotelA, hotelB) => {
+      const valA = getSortValue(hotelA);
+      const valB = getSortValue(hotelB);
+      if (valA === valB) return 0;
+      if (valA === null || valA === undefined) return 1;
+      if (valB === null || valB === undefined) return -1;
+      return (valA > valB) === (sortData.get('order') === 'ASC') ? 1 : -1;
+    };
+  }
+
+  getCheapestAmount(hotel) {
+    return hotel.cheapestRate.price.amount;
+  }
+
+  getReviewScore(hotel) {
+    return parseInt(hotel.reviewScore);
+  }
+
+  getStarRating(hotel) {
+    return Math.round(hotel.star);
   }
 
   hasTheStarsAligned(hotel, starRatings = new List()) {
