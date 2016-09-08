0910d4eb90b103c511580f4b99f3685543a719cf
Tom Hoang <hoangdinhphuong89@gmail.com>
2016-09-08 22:53:12 +0800

    Clean up starRating

--- app/sdk/HotelSearchEngine/filterHotels.js
+++ app/sdk/HotelSearchEngine/filterHotels.js
@@ -1,10 +1,10 @@
-export const matchStarRatingFilter = (hotel, filter) => {
+export const filterByStarRating = (hotel, filter) => {
   const starRatings = filter.get('starRatings');
   return starRatings.isEmpty() || starRatings.getIn([Math.floor(hotel.star).toString(), 'selected']);
 };
 
 const filterHotels = (hotels, filter) => {  // eslint-disable-line arrow-body-style
-  return hotels.filter(hotel => matchStarRatingFilter(hotel, filter));
+  return hotels.filter(hotel => filterByStarRating(hotel, filter));
 };
 
 export default filterHotels;
--- app/sdk/HotelSearchEngine/tests/filterHotels.test.js
+++ app/sdk/HotelSearchEngine/tests/filterHotels.test.js
@@ -1,8 +1,8 @@
-import { matchStarRatingFilter } from '../filterHotels';
+import { filterByStarRating } from '../filterHotels';
 import { fromJS } from 'immutable';
 
 describe('filterHotels', () => {
-  describe('#matchStarRatingFilter', () => {
+  describe('#filterByStarRating', () => {
     it('returns true for hotel that has matched star', () => {
       const hotel = { star: 2 };
       const filter = fromJS({
@@ -12,7 +12,7 @@ describe('filterHotels', () => {
           },
         },
       });
-      expect(matchStarRatingFilter(hotel, filter)).to.equal(true);
+      expect(filterByStarRating(hotel, filter)).to.equal(true);
     });
 
     it('returns false for hotel that does not have matched star', () => {
@@ -24,7 +24,7 @@ describe('filterHotels', () => {
           },
         },
       });
-      expect(matchStarRatingFilter(hotel, filter)).to.equal(false);
+      expect(filterByStarRating(hotel, filter)).to.equal(false);
     });
   });
 });
