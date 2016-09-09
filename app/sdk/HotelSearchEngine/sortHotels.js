9242752a0e675f9329d0fc68d28a494459aa142d
inyigo <inigo@wego.com>
2016-09-09 22:26:51 +0800

    sortHotels

---
+++ app/sdk/HotelSearchEngine/sortHotels.js
@@ -0,0 +1,38 @@
+
+const getCheapestAmount = (hotel) => hotel.cheapestRate.price.amount;
+
+const getReviewScore = (hotel) => parseInt(hotel.reviewScore);
+
+const getStarRating = (hotel) => Math.round(hotel.star);
+
+const propertyGetter = (property) => {
+  switch (property) {
+    case 'PRICE':
+      return getCheapestAmount;
+    case 'REVIEW_SCORE':
+      return getReviewScore;
+    case 'STAR_RATING':
+      return getStarRating;
+    default:
+      return getCheapestAmount;
+  }
+};
+
+const sortBy = (sortData) => {
+  const getSortValue = propertyGetter(sortData.get('property'));
+  return (hotelA, hotelB) => {
+    const valA = getSortValue(hotelA);
+    const valB = getSortValue(hotelB);
+    if (valA === valB) return 0;
+    if (valA === null || valA === undefined) return 1;
+    if (valB === null || valB === undefined) return -1;
+    return (valA > valB) === (sortData.get('order') === 'ASC') ? 1 : -1;
+  };
+};
+
+const sortHotels = (hotels, sortData) => {
+  const sorter = sortBy(sortData);
+  return hotels.sort(sorter);
+};
+
+export default sortHotels;
