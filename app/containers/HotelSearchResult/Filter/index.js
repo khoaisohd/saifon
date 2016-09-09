cfc41ffd585041cc544b6d52d2f0671a7927ea46
Tom Hoang <hoangdinhphuong89@gmail.com>
2016-09-09 22:38:29 +0800

    filterByPrice when user after change

--- app/containers/HotelSearchResult/Filter/index.js
+++ app/containers/HotelSearchResult/Filter/index.js
@@ -9,17 +9,22 @@ import Checkbox from 'components/Checkbox';
 import StarRating from 'components/StarRating';
 
 class Filter extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    const minPriceValue = Math.max(props.filter.getIn(['minPrice', 'value']), props.filter.getIn(['minPrice', 'threshold']));
+    const maxPriceValue = Math.min(props.filter.getIn(['maxPrice', 'value']), props.filter.getIn(['maxPrice', 'threshold']));
+    this.state = {
+      minPriceValue,
+      maxPriceValue,
+    };
+  }
+
   handleApplyClick() {
     this.context.router.goBack();
   }
 
   render() {
     const { filter, toggleStarRatingFilter, filterByPrice } = this.props;
-    const minPriceThreshold = filter.getIn(['minPrice', 'threshold']);
-    const minPriceValue = Math.max(filter.getIn(['minPrice', 'value']), minPriceThreshold);
-    const maxPriceThreshold = filter.getIn(['maxPrice', 'threshold']);
-    const maxPriceValue = Math.min(filter.getIn(['maxPrice', 'value']), maxPriceThreshold);
-
     return (
       <div>
         <div className={styles.toolbar}>
@@ -27,6 +32,19 @@ class Filter extends React.Component { // eslint-disable-line react/prefer-state
           <i className={appStyles.cancelIcon} onClick={this.context.router.goBack} />
         </div>
         <div className={appStyles.containerBody}>
+          Price
+          <div className={styles.inputRange}>
+            <span>{this.state.minPriceValue}</span> -> <span>{this.state.maxPriceValue}</span>
+            <ReactSlider
+              withBars value={[this.state.minPriceValue, this.state.maxPriceValue]}
+              step={5}
+              min={filter.getIn(['minPrice', 'threshold'])}
+              max={filter.getIn(['maxPrice', 'threshold'])}
+              onChange={value => this.setState({ minPriceValue: value[0], maxPriceValue: value[1] })}
+              onAfterChange={value => filterByPrice(value[0], value[1])}
+            />
+          </div>
+
           Star
           <div>
             {
@@ -38,18 +56,6 @@ class Filter extends React.Component { // eslint-disable-line react/prefer-state
                 </div>
               )
             }
-          </div>
-
-          Price
-          <div className={styles.inputRange}>
-            <span>{minPriceValue}</span> -> <span>{maxPriceValue}</span>
-            <ReactSlider
-              withBars value={[minPriceValue, maxPriceValue]}
-              step={5}
-              min={minPriceThreshold}
-              max={maxPriceThreshold}
-              onChange={value => filterByPrice(value[0], value[1])}
-            />
           </div>
         </div>
       </div>
