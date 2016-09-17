c38d2843344be76fcda28b3dd9d3dd445ab55915
Tom Hoang <hoangdinhphuong89@gmail.com>
2016-09-17 23:34:18 +0800

    Lint

--- app/components/shared/LegacyInk/index.js
+++ app/components/shared/LegacyInk/index.js
@@ -1,8 +1,6 @@
-/* eslint-disable */
-
 import styles from './styles.css';
 import React, { Component, PropTypes } from 'react';
-import { getBlotOpacity, getRadius }  from './equations';
+import { getBlotOpacity, getRadius } from './equations';
 
 import { TWO_PI, OPACITY, ON_CLICK_DURATION, DURATION } from './constants';
 
@@ -13,36 +11,83 @@ class Ink extends Component {
       height: 0,
       width: 0,
     };
-    this._frame = null;
-    this._blots = [];
-    this._waving = false;
+    this.frame = null;
+    this.blots = [];
+    this.waving = false;
   }
 
   componentWillUnmount() {
     this.stopWaving();
   }
 
-  handleClick(e) {
-    this.setupCanvas();
-    this._blots.push(this.createBlot(e.clientX, e.clientY));
-    this.starWaving();
-    setTimeout(this.props.onClick, ON_CLICK_DURATION);
-  }
-
   setupCanvas() {
-    if (!this._setupCanvas) {
-      const { top, bottom, left, right } = this.refs.canvas.getBoundingClientRect();
+    if (!this.setupCanvasAlready) {
+      const { top, bottom, left, right } = this.canvas.getBoundingClientRect();
       this.setState({
         height: bottom - top,
         width: right - left,
       });
-      this._setupCanvas = true;
+      this.setupCanvasAlready = true;
+    }
+  }
+
+  getCanvasContext() {
+    if (!this.canvasContext) {
+      let el = this.canvas;
+
+      // 0.13 support
+      if (el instanceof window.HTMLCanvasElement === false) {
+        el = el.getDOMNode();
+      }
+
+      this.canvasContext = el.getContext('2d');
+    }
+    return this.canvasContext;
+  }
+
+  drawBlot(blot) {
+    const { x, y } = blot;
+    const ctx = this.getCanvasContext();
+
+    ctx.globalAlpha = getBlotOpacity(blot, OPACITY);
+    ctx.beginPath();
+    ctx.arc(x, y, getRadius(blot), 0, TWO_PI);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  wave() {
+    this.blots = this.blots.filter(blot => Date.now() - blot.created < DURATION);
+    if (this.blots.length) {
+      const ctx = this.getCanvasContext();
+      ctx.save();
+      ctx.clearRect(0, 0, this.state.width, this.state.height);
+      this.blots.forEach(blot => this.drawBlot(blot));
+      ctx.restore();
+
+      this.frame = requestAnimationFrame(this.wave.bind(this));
+    } else {
+      this.stopWaving();
+    }
+  }
+
+  stopWaving() {
+    if (this.waving) {
+      this.waving = false;
+      cancelAnimationFrame(this.frame);
+    }
+  }
+
+  starWaving() {
+    if (!this.waving) {
+      this.waving = true;
+      this.wave();
     }
   }
 
   createBlot(clientX, clientY) {
-    let el = this.refs.canvas;
-    let { top, bottom, left, right } = el.getBoundingClientRect();
+    const el = this.canvas;
+    const { top, bottom, left, right } = el.getBoundingClientRect();
     const height = bottom - top;
     const width = right - left;
 
@@ -50,84 +95,27 @@ class Ink extends Component {
       created: Date.now(),
       radius: Math.max(height, width),
       x: clientX - left,
-      y: clientY - top
+      y: clientY - top,
     };
   }
 
-  starWaving() {
-    if (!this._waving) {
-      this._waving = true;
-      this.wave();
-    }
-  }
-
-  stopWaving() {
-    if (this._waving) {
-      this._waving = false;
-      cancelAnimationFrame(this._frame);
-    }
-  }
-
-  wave() {
-    this._blots = this._blots.filter(blot => Date.now() - blot.created < DURATION);
-    if (this._blots.length) {
-      let { height, width } = this.state;
-
-      const ctx = this.getCanvasContext();
-
-      ctx.save();
-
-      ctx.clearRect(0, 0, width, height);
-
-      this._blots.forEach(blot => this.drawBlot(blot));
-
-      ctx.restore();
-
-      this._frame = requestAnimationFrame(this.wave.bind(this));
-    } else {
-      this.stopWaving();
-    }
-  }
-
-  drawBlot(blot) {
-    let { x, y } = blot;
-
-    const ctx = this.getCanvasContext();
-
-    ctx.globalAlpha = getBlotOpacity(blot, OPACITY);
-
-    ctx.beginPath();
-
-    ctx.arc(x, y, getRadius(blot), 0, TWO_PI);
-
-    ctx.closePath();
-    ctx.fill()
-  }
-
-  getCanvasContext() {
-    if (!this._canvasContext) {
-      let el = this.refs.canvas
-
-      // 0.13 support
-      if (el instanceof window.HTMLCanvasElement === false) {
-        el = el.getDOMNode()
-      }
-
-      this._canvasContext = el.getContext('2d');
-    }
-    return this._canvasContext;
+  handleClick(e) {
+    this.setupCanvas();
+    this.blots.push(this.createBlot(e.clientX, e.clientY));
+    this.starWaving();
+    setTimeout(this.props.onClick, ON_CLICK_DURATION);
   }
 
   render() {
     return (
       <canvas
         className={styles.ink}
-        ref="canvas"
-        height={ this.state.height }
-        width={ this.state.width }
+        ref={component => { this.canvas = component; }}
+        height={this.state.height}
+        width={this.state.width}
         onClick={this.handleClick.bind(this)}
       />
-    )
+    );
   }
 }
 
@@ -136,5 +124,3 @@ Ink.propTypes = {
 };
 
 export default Ink;
-
-/* eslint-enable */
