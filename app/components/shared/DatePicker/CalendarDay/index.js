41f3103058d5a8c1759c3187e7e08eb99f43e9c6
inyigo <inigo@wego.com>
2016-09-21 00:30:07 +0800

    initial calendar day with styling

---
+++ app/components/shared/DatePicker/CalendarDay/index.js
@@ -0,0 +1,35 @@
+import React, { PropTypes } from 'react';
+import moment from 'moment';
+import styles from './index.css';
+
+const CalendarDay = (props) => {
+  const { day } = props;
+  let className;
+  if (props.selected) {
+    className = styles.selected;
+  } else if (props.highlighted) {
+    className = styles.highlited;
+  } else if (props.holiday) {
+    className = styles.holiday;
+  } else if (props.weekend) {
+    className = styles.weekend;
+  } else {
+    className = styles.day;
+  }
+  return (
+    <div className={className}>
+      {day.format('D')}
+    </div>
+  );
+};
+
+CalendarDay.propTypes = {
+  day: PropTypes.object,
+};
+
+CalendarDay.defaultProps = {
+  day: moment(),
+};
+
+export default CalendarDay;
+
---
+++ app/components/shared/DatePicker/CalendarDay/stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook'; // eslint-disable-line
+import CalendarDay from './index';
+
+storiesOf('CalendarDay', module)
+  .add('normal day', () => (
+    <CalendarDay  />
+  ))
+  .add('selected holiday', () => (
+    <CalendarDay selected />
+  ))
+  .add('holiday', () => (
+    <CalendarDay holiday />
+  ))
+  .add('highlighted day', () => (
+    <CalendarDay highlighted />
+  ))
+  .add('disabled day', () => (
+    <CalendarDay disabled />
+  ))
+  .add('weekend', () => (
+    <CalendarDay weekend />
+  ));
