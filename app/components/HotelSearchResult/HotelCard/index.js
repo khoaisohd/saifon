faedc0f38a52598664602290302232a99545c932
Tom Hoang <hoangdinhphuong89@gmail.com>
2016-09-26 08:30:44 +0800

    Hotel Card Details styling

--- app/components/HotelSearchResult/HotelCard/index.js
+++ app/components/HotelSearchResult/HotelCard/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import styles from './index.css';
+import StarRating from 'components/shared/StarRating';
+
+const starViews = {
+  1.0: <StarRating value={1.0} />,
+  2.0: <StarRating value={2.0} />,
+  3.0: <StarRating value={3.0} />,
+  4.0: <StarRating value={4.0} />,
+  5.0: <StarRating value={5.0} />,
+};
 
 const HotelCard = props => {
   const { hotel } = props;
@@ -12,8 +21,22 @@ const HotelCard = props => {
       <div className={styles.summary}>
         <div className={styles.name}>{hotel.name}</div>
         <div className={styles.address}>{hotel.districtName}</div>
-        <div>{hotel.reviewDescription} - {hotel.reviewScore}</div>
-        <div>S${hotel.cheapestRate.price.amount}</div>
+        <div className={styles.rating}>
+          {starViews[hotel.star]}
+        </div>
+        <div className={styles.reviewRate}>
+          <span className={`${styles.score}`}>{hotel.reviewScore}</span>
+          <div className={styles.subreview}>
+            <span className={styles.sentiment}>{hotel.reviewDescription}</span>
+            <span className={styles.reviewCount}>
+              {hotel.reviewCount} reviews
+            </span>
+          </div>
+          <div className={styles.rate}>
+            <span className={styles.rateCurrencySymbol}>S$</span>
+            <span className={styles.rateAmount}>{hotel.cheapestRate.price.amount}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
